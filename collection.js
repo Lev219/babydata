621a51218d37c5b7960d737aff7e7c55517154c4
Lev219 <lizhaolei2021@outlook.com>
2023-05-08 23:24:57 -0400

    1

---
+++ collection.js
@@ -0,0 +1,26 @@
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    
+    const clickedButtonValue = event.target.getAttribute('data-value');
+    let itemsToHide = document.querySelectorAll(`.gif-container:not(.${clickedButtonValue})`);
+    let itemsToShow = document.querySelectorAll(`.gif-container.${clickedButtonValue}`);
+    console.log(itemsToHide)
+   
+    itemsToHide.forEach(function(el) {
+      el.classList.add('hide');
+      el.classList.remove('show');
+    });
+    itemsToShow.forEach(function(el) {
+      el.classList.remove('hide');
+      el.classList.add('show'); 
+    });
+  })
+});
+
+$('.button').click(function() {
+  $('.button').removeClass('active');  
+  $(this).addClass('active');  
+});
